28a22b0cad89baac27185d2c413b49412c4c0d83
trishaagaba <agabatrisha@gmail.com>
2023-08-07 18:02:33 +0300

    Created the models for the user side

--- Recess_Project_Template/src/group_work/FeedbackSystem/userside/Authentication/models.py
+++ Recess_Project_Template/src/group_work/FeedbackSystem/userside/Authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Create your models here.
+class Student(models.Model):
+    st_name=models.CharField(max_length=50,unique=True)
+    # st_id=models.CharField(max_length=50,null=False)
+    # st_email=models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.st_name
