3a7bf15996c02728b84b325aed842f655a028668
“MarthaHellen” <martnahe@gmail.com>
2023-08-08 17:08:48 +0300

    User Authentication Model

--- Recess_Project_Template/src/group_work/FeedbackSystem/userside/Authentication/models.py
+++ Recess_Project_Template/src/group_work/FeedbackSystem/userside/Authentication/models.py
@@ -5,9 +5,21 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    st_name=models.CharField(max_length=50,unique=True)
+    username=models.CharField(max_length=50, unique=True)
     # st_id=models.CharField(max_length=50,null=False)
     # st_email=models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
+    student_number = models.CharField(max_length=10, unique=True)
+    YEAR_CHOICES = [
+        ('One', 'Year One'),
+        ('Two', 'Year Two'),
+        ('Three', 'Year Three'),
+        ('Four', 'Year Four'),
+    ]
+    year = models.CharField(max_length=10, choices=YEAR_CHOICES)
 
     def __str__(self):
-        return self.st_name
+        return self.username  #
+
+    # def __str__(self):
+    #     return self.st_name
