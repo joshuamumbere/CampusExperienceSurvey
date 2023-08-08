d70020a4a9f52e0bc59b0bc92f3c6fbd3070116d
arihoconrad <conradariho3@gmail.com>
2023-08-08 10:34:52 +0300

    Editted forms
    New forms made using django crispy forms package for faster development

--- Recess_Project_Template/src/group_work/FeedbackSystem/forms/app/urls.py
+++ Recess_Project_Template/src/group_work/FeedbackSystem/forms/app/urls.py
@@ -1,5 +1,9 @@
 # app/urls.py
 
+# feedback_app/urls.py
+
+# FeedbackSystem/app/urls.py
+
 from django.urls import path
 from . import views
 
@@ -7,4 +11,6 @@ urlpatterns = [
     path('course_feedback/', views.course_feedback, name='course_feedback'),
     path('instructor_feedback/', views.instructor_feedback, name='instructor_feedback'),
     path('campus_feedback/', views.campus_feedback, name='campus_feedback'),
+    path('thank_you/', views.thank_you, name='thank_you'),
+    path('submit_course_feedback/', views.submit_course_feedback, name='submit_course_feedback'),
 ]
